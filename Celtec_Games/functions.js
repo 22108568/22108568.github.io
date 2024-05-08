2de84bad5f17f1d5f7d6ff38b68bfdc056a07618
RumBulG <89697036+22108568@users.noreply.github.com>
2024-05-08 13:22:19 +0100

    Delete Celtec_Games directory

--- Celtec_Games/functions.js
+++
@@ -1,8 +0,0 @@
-const menuToggle = document.getElementById('menuToggle');
-const navigation = document.getElementById('navigation');
-const banner = document.getElementById('banner');
-
-menuToggle.addEventListener('click', () => {
-navigation.classList.toggle('active');
-banner.classList.toggle('active');
-});
